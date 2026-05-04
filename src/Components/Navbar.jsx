23168385ef9a476569063885caf015ab12dfb267
Ardox <ardox@axos-project.com>
2026-05-04 13:55:43 +0200

    Add actual icons in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import '../css/navbar.css';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Home, MyLocation, WaterDrop, AssignmentLate } from '@mui/icons-material';
 
 export default function Navrail() {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const pages = [
+    { name: 'Dashboard', icon: <Home /> },
+    { name: 'Parcelles', icon: <MyLocation /> },
+    { name: 'Irrigation', icon: <WaterDrop /> },
+    { name: 'Alertes', icon: <AssignmentLate /> }
+  ];
+
   return (
     <nav className="nav-container">
-      {[...Array(4)].map((_, index) => (
+      {pages.map((page, index) => (
         <button
           key={index}
           className={`nav-item ${activeIndex === index ? 'active' : ''}`}
           onClick={() => setActiveIndex(index)}
         >
-          <AddCircleIcon />
+          {page.icon}
         </button>
       ))}
     </nav>
